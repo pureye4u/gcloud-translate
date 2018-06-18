491c8a38faccf07f5fbd1fa102dc1c9dd0019db4
pureye4u <pureye4u@gmail.com>
2018-06-18 12:20:24 +0900

    Build setting

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:9.20
+MAINTAINER pureye4u<pureye4u@gmail.com>
+
+RUN mkdir -p /app
+WORKDIR /app
+ADD . /app
+ADD ./credentials.json /app
+RUN npm install
+RUN export GOOGLE_APPLICATION_CREDENTIALS="./credentials.json"
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Variables
+PROJECT_NAME=gcloud-translate
+TARGET=master
+TARGET_PORT=3000
+CONTAINER_NAME=${PROJECT_NAME}-${TARGET}
+IMAGE_NAME=${PROJECT_NAME}:${TARGET}
+
+echo "TARGET: ${TARGET}"
+echo "CONTAINER: ${PROJECT_NAME}"
+echo "IMAGE: ${IMAGE_NAME}"
+
+git checkout --track origin/${TARGET}
+
+git pull
+
+cp ~/credentials/google/29cm-global.json .
+
+docker stop ${CONTAINER_NAME}
+
+docker rm ${CONTAINER_NAME}
+
+docker rmi ${IMAGE_NAME}
+
+docker build -t ${IMAGE_NAME} .
+
+docker run --name ${CONTAINER_NAME} -d -p ${TARGET_PORT}:3000 ${IMAGE_NAME}
